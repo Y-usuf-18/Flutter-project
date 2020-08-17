74ec8de25dcf4146f030f3b2e11b1aae9742b373
Y-usuf-18 <yusufayann.2134@gmail.com>
2020-08-17 16:02:41 +0300

    first commit

--- lib/devam.dart
+++ lib/devam.dart
@@ -20,18 +20,28 @@ class DevamButton extends StatelessWidget{
           backgroundColor: Color(0xff6200ee),
         ),
         body: Center(
-          child: SizedBox(
-            width: 200,
-            height: 50,
-            child: RaisedButton(
-              onPressed: () {},
-              child: Text(
-                "DEVAM",
-                style: TextStyle(
-                  fontSize: 19,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+
+            children: [
+              Text("Üniversite hazırlık başta olmak üzere hedefine ulaşmak isteyen her lise öğrencisinin kendini takip edebilmek için kullanacağı sanal rehber uygulaması. Uygulama öncelikle öğrencinin mevcut durumunu analiz edecek ve hedefine ulaşması için ne yapması gerektiği konusunda yönlendirecek",style: TextStyle(fontSize: 13),textAlign: TextAlign.center,),
+              SizedBox(
+                width: 200,
+                height: 50,
+                child: RaisedButton(
+                  color: Color(0xff6200ee),
+                  onPressed: () {},
+                  child: Text(
+                    "DEVAM",
+                    style: TextStyle(
+                      fontSize: 19,color: Colors.white
+
+                    ),
+                  ),
                 ),
               ),
-            ),
+            ],
           ),
         ),
       ),
