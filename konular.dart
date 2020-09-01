631402fa45ca44d7656f68af90f770dc2c1948e9
Yusuf <68030534+Y-usuf-18@users.noreply.github.com>
2020-09-01 17:41:58 +0300

    Add files via upload

---
+++ konular.dart
@@ -0,0 +1,131 @@
+import 'package:akilliRehber/main.dart';
+import 'package:akilliRehber/models/siniflar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+
+
+
+
+
+
+
+class KonuListesi extends StatefulWidget{
+  final Dersler dersadi;
+
+  KonuListesi({this.dersadi});
+
+  @override
+  _KonuListesiState createState() =>_KonuListesiState();
+}
+
+class _KonuListesiState extends State<KonuListesi>{
+  bool checkBoxValue1 = false;
+
+
+
+
+
+
+  List<Sinif> tumSiniflar;
+  @override void initState() {
+    super.initState();
+    veriOku().then((gelenSiniflar){
+      setState(() {
+        tumSiniflar = gelenSiniflar;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Dersler',
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Siniflar()),
+            ),
+          ),
+          backgroundColor: Color(0xff6200ee),
+          title: Text("AKILLI REHBER"),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+
+          child: Column(
+
+            children:[
+              new Container(
+                child:  Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(widget.dersadi.ders,
+                        style: TextStyle(fontSize: 28, height: 2 ,fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              new Container(
+                alignment: Alignment.center,
+                child: FutureBuilder(
+                    future: veriOku(),
+                    builder:(context, sonuc){
+
+                      if(sonuc.hasData){
+                        tumSiniflar = sonuc.data;
+                        return ListView.builder(
+                          physics: NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            return CheckboxListTile(
+                              value: checkBoxValue1,
+                              onChanged: (bool value) {
+                                print(value);
+                                setState(() {
+                                  checkBoxValue1= value;
+                                });
+                              },
+
+                              title: Text(widget.dersadi.konular[index].toString(),style: TextStyle(
+                                fontSize: 16,
+                              ),
+                              ),
+
+                            );
+
+
+                          },
+                          itemCount: widget.dersadi.konular.length,
+                        );
+                      }else{
+                        return Center(child: CircularProgressIndicator(),);
+                      }
+                    }),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+  Future<List> veriOku() async{
+    var gelenJson = await DefaultAssetBundle.of(context).loadString("assets/data/mufredat.json");
+    List<Sinif> sinifListesi = (json.decode(gelenJson) as List).map((mapYapisi) => Sinif.fromJson(mapYapisi)).toList();
+    for ( int a=0; a<sinifListesi[0].dersler[0].konular.length; a++ ) {
+      debugPrint("Ders: " + sinifListesi[0].dersler[0].konular[a].toString());
+    }
+    return sinifListesi;
+
+  }
+}
+
+
